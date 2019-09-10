1f65f50f708ef9d88635b832806f48492d618894
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-10 17:21:06 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,4 +2,15 @@ require 'pry'
 
 def my_find(collection)
 
+  i = 0
+  found = []
+  while i < collection.size
+    if yield(collection[i])
+      found << collection[i]
+    end
+    i += 1
+  
+  end
+
+
 end
