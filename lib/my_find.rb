e36ef60faf5da381b8da1e8eff310b2e61ba8c61
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-10 17:26:29 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,39 @@
 require 'pry'
 
+# def my_find(collection)
+
+#   i = 0
+#   found = []
+#   while i < collection.size
+#     if yield(collection[i])
+#       found << collection[i]
+#     end
+#     i += 1
+  
+#   end
+
+#   # if found.empty?
+#   #   return nil
+#   # else
+#   #   found
+#   # end
+  
+#   found.empty? ? nil : found
+# end
+
+
 def my_find(collection)
+  
+  i = 0 
+  
+  while i < collection.size
+  
+    if yield(collection[i])
+      return collection[i]
+    end
+    
+    i += 1
+  
+  end
 
 end
